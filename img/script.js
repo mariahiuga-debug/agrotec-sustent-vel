79b7cca0c9fe212940e0edbb38df16e9d800d596
mariahiuga-debug <mariah.iuga@escola.pr.gov.br>
2026-06-10 15:28:53 -0300

    Delete img/script.js

--- img/script.js
+++
@@ -1,9 +0,0 @@
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('.content-section');
-    sections.forEach(section => {
-        section.classList.remove('active');
-    });
-
-    const selected = document.getElementById(sectionId);
-    selected.classList.add('active');
-}

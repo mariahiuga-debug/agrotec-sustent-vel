40b1fc53390d5e1238e2dd2efd7ac232ebef03cc
mariahiuga-debug <mariah.iuga@escola.pr.gov.br>
2026-06-10 13:55:55 -0300

    Update script.js

--- img/script.js
+++ img/script.js
@@ -1,1 +1,9 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
 
+    const selected = document.getElementById(sectionId);
+    selected.classList.add('active');
+}
